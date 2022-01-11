d12a1a5d9e985542219bf64ebd990cc10a2c5bcc
Edgardo Carreras <edgardo.carreras@galaxydigital.io>
2022-01-11 18:27:33 -0400

    Add game struct and new game function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-mod main_test;
-
 fn main() {
     println!("Hello, world!");
+}
+
+
+pub struct Game {
+   is_over: bool
+}
+
+pub fn new_game() -> Game {
+    Game {
+        is_over: false
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{new_game};
+
+    #[test]
+    fn is_not_over() {
+        let game = new_game();
+        assert!(!game.is_over)
+    }
 }
