4a7108c6a4ea1dd58647c26791f1bb3e1eb8a3d9
Edgardo Carreras <edgardo.carreras@galaxydigital.io>
2022-01-11 18:54:38 -0400

    Add board state and play method on game

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,51 @@ fn main() {
 }
 
 
+const X: u8 = 1;
+
+#[derive(Debug)]
 pub struct Game {
-   is_over: bool
+   is_over: bool,
+   board: [u8; 9]
+}
+
+impl Game {
+    fn play(&mut self, space: usize) {
+        self.board[space] = X;
+    }
 }
 
 pub fn new_game() -> Game {
     Game {
-        is_over: false
+        is_over: false,
+        board: [0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
 }
 
 #[cfg(test)]
-mod test {
-    use crate::{new_game};
+mod new_game {
+    use crate::{new_game, X};
 
     #[test]
     fn is_not_over() {
         let game = new_game();
         assert!(!game.is_over)
     }
+
+    #[test]
+    fn has_empty_board() {
+        let game = new_game();
+        let board = game.board;
+        for space in board {
+            assert_eq!(space, 0)
+        }
+    }
+
+    #[test]
+    fn x_plays_first() {
+        let mut game = new_game();
+        game.play(0);
+        assert_eq!(game.board[0], X)
+    }
+
 }
