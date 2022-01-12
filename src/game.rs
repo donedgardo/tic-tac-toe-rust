795bacc83dfa98eaea7b0164a2b2e40cc3b17e01
Edgardo Carreras <edgardo.carreras@galaxydigital.io>
2022-01-11 21:39:24 -0400

    Add taken position handeling

--- src/game.rs
+++ src/game.rs
@@ -5,6 +5,7 @@ use crate::play_markers::PlayMarkers;
 pub struct Game {
     is_over: bool,
     board: HashMap<u8, PlayMarkers>,
+    error: Option<String>,
 }
 
 impl Game {
@@ -12,9 +13,19 @@ impl Game {
         Self {
             is_over: false,
             board: HashMap::new(),
+            error: None,
         }
     }
     pub fn play(&mut self, space: u8) {
+        if self.board.contains_key(&space) {
+            let error = format!("Can't play in position {}, as it has been already played.", space);
+            self.error = Some(error);
+        } else {
+            self.play_board_space(space)
+        }
+    }
+
+    fn play_board_space(&mut self, space: u8) {
         if self.board.len() % 2 == 0 {
             self.board.insert(space, PlayMarkers::X);
         } else {
@@ -54,5 +65,23 @@ mod new_game {
         game.play(0);
         game.play(1);
         assert_eq!(game.board.get(&1), Some(&PlayMarkers::O));
+    }
+
+    #[test]
+    fn error_when_playing_in_taken_position_0() {
+        let mut game = Game::new();
+        game.play(0);
+        game.play(0);
+        let expected_error = "Can't play in position 0, as it has been already played.".to_string();
+        assert_eq!(game.error, Some(expected_error))
+    }
+
+    #[test]
+    fn error_when_playing_in_taken_position_1() {
+        let mut game = Game::new();
+        game.play(1);
+        game.play(1);
+        let expected_error = "Can't play in position 1, as it has been already played.".to_string();
+        assert_eq!(game.error, Some(expected_error))
     }
 }
