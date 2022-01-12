f75c9de4040052190fd2e91b7362a87e1cfc1925
Edgardo Carreras <edgardo.carreras@galaxydigital.io>
2022-01-12 16:24:02 -0400

    print board

--- src/cli_game_manager.rs
+++ src/cli_game_manager.rs
@@ -14,21 +14,36 @@ impl CLIGameManager {
     }
 
     pub fn print(self, mut writer: impl std::io::Write ) {
-        writeln!(writer, "{}", "Hello World");
+        let mut output =  self.format_board_display();
+        output += "X's turn!\n";
+        let _result = writeln!(writer, "{}", output);
     }
+
+    pub fn format_board_display(self) -> String {
+        let mut board_display = String::new();
+        for space in 1..10 {
+            if space % 3 != 0 {
+                board_display += "_|";
+            } else {
+                board_display += "_\n";
+            }
+        }
+        board_display
+    }
+
+
 }
 
 #[cfg(test)]
 mod cli_game {
     use crate::cli_game_manager::CLIGameManager;
-    use crate::game::Game;
 
     #[test]
     fn prints_welcome() {
         let cli = CLIGameManager::new(true);
         let mut result = Vec::new();
         cli.print(&mut result);
-        assert_eq!(result, b"Hello World\n");
+        assert_eq!(result, b"_|_|_\n_|_|_\n_|_|_\nX's turn!\n\n");
     }
 
 
