795804f142a92ad418aac2886d4664b72033689d
Edgardo Carreras <edgardo.carreras@galaxydigital.io>
2022-01-17 15:23:51 -0400

    ai gets obvious winning move

--- src/ai2.rs
+++ src/ai2.rs
@@ -1,10 +1,26 @@
 use rand::seq::SliceRandom;
 use crate::Game;
 
+
 pub fn get_best_move_2(game: &Game) -> u8
 {
     let moves = game.get_available_plays();
-    *moves.choose(&mut rand::thread_rng()).unwrap()
+    let mut best_move: u8 = *moves.choose(&mut rand::thread_rng()).unwrap();
+    if should_min_max(moves.len()) {
+        for board_space in moves {
+            let mut game_copy = game.copy();
+            game_copy.play(board_space);
+            if game_copy.is_over {
+                best_move = board_space;
+                break;
+            }
+        }
+    }
+    best_move
+}
+
+fn should_min_max(available_move_count: usize) -> bool {
+    available_move_count < 9 && available_move_count > 1
 }
 
 #[cfg(test)]
@@ -13,7 +29,7 @@ mod ai_game_2 {
     use crate::Game;
 
     #[test]
-    fn get_random_first_move() {
+    fn plays_random_first_move() {
         let game = Game::new();
         let best_move = get_best_move_2(&game);
         let possible_moves: Vec<u8> = (0u8..9u8).collect();
@@ -21,7 +37,7 @@ mod ai_game_2 {
     }
 
     #[test]
-    fn get_only_move_available() {
+    fn plays_only_move_available() {
         let mut game = Game::new();
         for play in [0, 4, 2, 1, 7, 5, 3, 6] {
             game.play(play);
@@ -30,4 +46,13 @@ mod ai_game_2 {
         assert_eq!(8u8, best_move);
     }
 
+    #[test]
+    fn plays_obvious_winning_move() {
+        let mut game = Game::new();
+        for play in [0, 4, 6, 7] {
+            game.play(play);
+        }
+        let best_move = get_best_move_2(&game);
+        assert_eq!(3u8, best_move);
+    }
 }
